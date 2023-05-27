5a2d41f9b468ecc252d22bd7487d776f76670bd0
Fatih Deniz <deniz.fatih@hotmail.de>
2023-05-27 23:33:13 +0200

    Day 4 - Repositories & Services

--- src/main/java/com/schoolmanagement/controller/AdminController.java
+++ src/main/java/com/schoolmanagement/controller/AdminController.java
@@ -1,13 +1,32 @@
 package com.schoolmanagement.controller;
 
+import com.schoolmanagement.payload.request.AdminRequest;
+import com.schoolmanagement.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("admin")
 @RequiredArgsConstructor
 public class AdminController {
 
-    //Ödev: Not: save() service ve repo yazilmaya calisilacak. Admin kaydedecek
+    private final AdminService adminService;
+
+    //Not: save() *********************************************************************************************************************************
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody @Valid AdminRequest adminRequest){
+
+        return ResponseEntity.ok(adminService.save(adminRequest));
+    }
+
+    //Not: getAll() *********************************************************************************************************************************
+
+
+    //Not: delete() *********************************************************************************************************************************
 }
