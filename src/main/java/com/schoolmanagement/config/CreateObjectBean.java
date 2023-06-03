670bbc0a03dfa8a6f498b1dcd5590d27c2eb1827
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-03 23:08:42 +0200

    Day 9 - ViceDean Controller & Service

--- src/main/java/com/schoolmanagement/config/CreateObjectBean.java
+++ src/main/java/com/schoolmanagement/config/CreateObjectBean.java
@@ -1,19 +1,48 @@
 package com.schoolmanagement.config;
 
 import com.schoolmanagement.payload.dto.DeanDto;
+import com.schoolmanagement.payload.dto.ViceDeanDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //Custom Metodlar üzerinden metotlar olusturup Bean'leri SpringBoot'a göndermis oluyoruz
 public class CreateObjectBean {
 
-
     //Tek class üzerinden Bean'leri kontrol etmemizi sagliyoruz. Diger türlü @Component annotation'u yazmamiz gerekli
 
-   @Bean
-   public DeanDto deanDTO(){
+    @Bean
+    public DeanDto deanDTO() {
 
-       return  new DeanDto();
-   }
+        return new DeanDto();
+    }
+
+    @Bean
+    public ViceDeanDto viceDeanDTO() {
+
+        return new ViceDeanDto();
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
