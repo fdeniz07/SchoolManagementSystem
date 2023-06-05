138b59be0883157cf3cad7db5e0239a2ab6b5721
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-05 23:03:41 +0200

    Day 10 - EducationTerm Controller & Service & Repository

---
+++ src/main/java/com/schoolmanagement/payload/request/EducationTermRequest.java
@@ -0,0 +1,48 @@
+package com.schoolmanagement.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.schoolmanagement.entity.enums.Term;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder(toBuilder = true)
+public class EducationTermRequest implements Serializable {
+
+    @NotNull(message="Education Term must be not be empty")
+    private Term term;
+
+    @NotNull(message = "Start Date must not be empty")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @NotNull(message = "End Date must not be empty")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @NotNull(message = "Last Registration Date must not be empty")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private LocalDate lastRegistrationDate;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
