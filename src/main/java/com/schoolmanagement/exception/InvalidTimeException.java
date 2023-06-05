7e7b1382236df03b6808fdb087a82e516839c8d4
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-05 23:04:04 +0200

    Day 10 - EducationTerm Controller & Service & Repository

---
+++ src/main/java/com/schoolmanagement/exception/InvalidTimeException.java
@@ -0,0 +1,8 @@
+package com.schoolmanagement.exception;
+
+public class InvalidTimeException extends  RuntimeException{
+
+    public InvalidTimeException(String message) {
+        super(message);
+    }
+}
