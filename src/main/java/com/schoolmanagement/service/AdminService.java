6961ca2082b19aa288fa7514327df2d6cbba85d2
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-07 18:54:12 +0200

    Day 12 - Revision 1

--- src/main/java/com/schoolmanagement/service/AdminService.java
+++ src/main/java/com/schoolmanagement/service/AdminService.java
@@ -7,7 +7,9 @@ import com.schoolmanagement.payload.request.AdminRequest;
 import com.schoolmanagement.payload.response.AdminResponse;
 import com.schoolmanagement.payload.response.ResponseMessage;
 import com.schoolmanagement.repository.*;
+import com.schoolmanagement.utils.CheckUniqueFields;
 import com.schoolmanagement.utils.Messages;
+import com.schoolmanagement.utils.mappers.AdminDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,23 +25,19 @@ import java.util.Optional;
 public class AdminService {
 
     private final AdminRepository adminRepository;
-    private final StudentRepository studentRepository;
-    private final ViceDeanRepository viceDeanRepository;
-    private final DeanRepository deanRepository;
-    private final TeacherRepository teacherRepository;
-    private final GuestUserRepository guestUserRepository;
-
     private final UserRoleService userRoleService;
     private final PasswordEncoder passwordEncoder;
+    private final CheckUniqueFields checkUniqueFields; //Unique alanlarin kontrolü icin DI yapildi
+    private final AdminDto adminDto;
 
     // Not: save()  ********************************************************************************************************************************
     public ResponseMessage save(AdminRequest request) {
 
         // !!! Girilen username - ssn- phoneNumber unique mi kontrolu
-        checkDuplicate(request.getUsername(), request.getSsn(), request.getPhoneNumber());
+        checkUniqueFields.checkDuplicate(request.getUsername(), request.getSsn(), request.getPhoneNumber()); // sadelestirildi
 
         //!!! Admin nesnesini builder ile olusturuyoruz
-        Admin admin = createAdminForSave(request);
+        Admin admin = adminDto.createAdminForSave(request); //Not: dto - pojo --> mapper gerekli
         admin.setBuilt_in(false);
 
         //Admin nesnesinin built_il field'ini true'ye cekiyoruz --Trick nokta
@@ -47,112 +45,77 @@ public class AdminService {
             admin.setBuilt_in(true); //Admin olarak yazinca otomatik built_in oluyor
 
         //!!! admin rolü veriliyor
-        admin.setUserRole(userRoleService.getUserRole(RoleType.ADMIN)); // Rolü repositoryde degil service de atiyoruz ki dönen exception
-        // handle edilebilsin
+        admin.setUserRole(userRoleService.getUserRole(RoleType.ADMIN)); // Rolü repositoryde degil service de atiyoruz ki dönen exception handle edilebilsin
 
         //!!! Password encode  ediliyor (Security ile yazilir)
         admin.setPassword(passwordEncoder.encode(admin.getPassword()));
-
 
         Admin savedData = adminRepository.save(admin);
 
         return ResponseMessage.<AdminResponse>builder()
                 .message("Admin saved")
                 .httpStatus(HttpStatus.CREATED)
-                .object(createResponse(savedData)) //pojo - dto
+                .object(adminDto.createResponse(savedData)) //Not: pojo - dto --> mapper gerekli
                 .build();
     }
 
-    public void checkDuplicate(String username, String ssn, String phone) {
-        if (adminRepository.existsByUsername(username) ||
-                deanRepository.existsByUsername(username) ||
-                studentRepository.existsByUsername(username) ||
-                teacherRepository.existsByUsername(username) ||
-                viceDeanRepository.existsByUsername(username) ||
-                guestUserRepository.existsByUsername(username)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_USERNAME, username));
-        } else if (adminRepository.existsBySsn(ssn) ||
-                deanRepository.existsBySsn(ssn) ||
-                studentRepository.existsBySsn(ssn) ||
-                teacherRepository.existsBySsn(ssn) ||
-                viceDeanRepository.existsBySsn(ssn) ||
-                guestUserRepository.existsBySsn(ssn)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_SSN, ssn));
-        } else if (adminRepository.existsByPhoneNumber(phone) ||
-                deanRepository.existsByPhoneNumber(phone) ||
-                studentRepository.existsByPhoneNumber(phone) ||
-                teacherRepository.existsByPhoneNumber(phone) ||
-                viceDeanRepository.existsByPhoneNumber(phone) ||
-                guestUserRepository.existsByPhoneNumber(phone)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_PHONE_NUMBER, phone));
-        }
-    }
-
-    /*
-         ÖDEV -- Yukardaki duplicate methodunu 4 parametreli hale getirmek istersem ???  - Vararrgs
-
-       public void checkDuplicate2(String... values) {
-        String username = values[0];
-        String ssn = values[1];
-        String phone = values[2];
-        String email = values[3];
-
-        if (adminRepository.existsByUsername(username) || deanRepository.existsByUsername(username) ||
-                studentRepository.existsByUsername(username) || teacherRepository.existsByUsername(username) ||
-                viceDeanRepository.existsByUsername(username) || guestUserRepository.existsByUsername(username)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_USERNAME, username));
-        } else if (adminRepository.existsBySsn(ssn) || deanRepository.existsBySsn(ssn) ||
-                studentRepository.existsBySsn(ssn) || teacherRepository.existsBySsn(ssn) ||
-                viceDeanRepository.existsBySsn(ssn) || guestUserRepository.existsBySsn(ssn)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_SSN, ssn));
-        } else if (adminRepository.existsByPhoneNumber(phone) || deanRepository.existsByPhoneNumber(phone) ||
-                studentRepository.existsByPhoneNumber(phone) || teacherRepository.existsByPhoneNumber(phone) ||
-                viceDeanRepository.existsByPhoneNumber(phone) || guestUserRepository.existsByPhoneNumber(phone)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_PHONE_NUMBER, phone));
-        } else if (studentRepository.existsByEmail(email) || teacherRepository.existsByEmail(email)) {
-            throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_EMAIL, email));
-        }
-    }
-
-    */
-
-
-    protected Admin createAdminForSave(AdminRequest request) {
-
-        return Admin.builder()
-                .username(request.getUsername())
-                .name(request.getName())
-                .surname(request.getSurname())
-                .password(request.getPassword())
-                .ssn(request.getSsn())
-                .birthDay(request.getBirthDay())
-                .birthPlace(request.getBirthPlace())
-                .phoneNumber(request.getPhoneNumber())
-                .gender(request.getGender())
-                .build();
-    }
-
-    private AdminResponse createResponse(Admin admin) {
-
-        return AdminResponse.builder()
-                .userId(admin.getId())
-                .username(admin.getUsername())
-                .name(admin.getName())
-                .surname(admin.getSurname())
-                .phoneNumber(admin.getPhoneNumber())
-                .birthDay(admin.getBirthDay())
-                .birthPlace(admin.getBirthPlace())
-                .gender(admin.getGender())
-                .ssn(admin.getSsn())
-                .build();
-    }
-
+    /** Tasindi : ilk bastaki yapi asagidaki gibiydi --> utils-->ChechUniqueFields class ina tasindi
+     *
+     * public void checkDuplicate(String username, String ssn, String phone) {
+     *         if (adminRepository.existsByUsername(username) ||
+     *                 deanRepository.existsByUsername(username) ||
+     *                 studentRepository.existsByUsername(username) ||
+     *                 teacherRepository.existsByUsername(username) ||
+     *                 viceDeanRepository.existsByUsername(username) ||
+     *                 guestUserRepository.existsByUsername(username)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_USERNAME, username));
+     *         } else if (adminRepository.existsBySsn(ssn) ||
+     *                 deanRepository.existsBySsn(ssn) ||
+     *                 studentRepository.existsBySsn(ssn) ||
+     *                 teacherRepository.existsBySsn(ssn) ||
+     *                 viceDeanRepository.existsBySsn(ssn) ||
+     *                 guestUserRepository.existsBySsn(ssn)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_SSN, ssn));
+     *         } else if (adminRepository.existsByPhoneNumber(phone) ||
+     *                 deanRepository.existsByPhoneNumber(phone) ||
+     *                 studentRepository.existsByPhoneNumber(phone) ||
+     *                 teacherRepository.existsByPhoneNumber(phone) ||
+     *                 viceDeanRepository.existsByPhoneNumber(phone) ||
+     *                 guestUserRepository.existsByPhoneNumber(phone)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_PHONE_NUMBER, phone));
+     *         }
+     *     }
+     *
+     *     ÖDEV -- Yukardaki duplicate methodunu 4 parametreli hale getirmek istersem ???  - Vararrgs
+     *
+     *        public void checkDuplicate2(String... values) {
+     *         String username = values[0];
+     *         String ssn = values[1];
+     *         String phone = values[2];
+     *         String email = values[3];
+     *
+     *         if (adminRepository.existsByUsername(username) || deanRepository.existsByUsername(username) ||
+     *                 studentRepository.existsByUsername(username) || teacherRepository.existsByUsername(username) ||
+     *                 viceDeanRepository.existsByUsername(username) || guestUserRepository.existsByUsername(username)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_USERNAME, username));
+     *         } else if (adminRepository.existsBySsn(ssn) || deanRepository.existsBySsn(ssn) ||
+     *                 studentRepository.existsBySsn(ssn) || teacherRepository.existsBySsn(ssn) ||
+     *                 viceDeanRepository.existsBySsn(ssn) || guestUserRepository.existsBySsn(ssn)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_SSN, ssn));
+     *         } else if (adminRepository.existsByPhoneNumber(phone) || deanRepository.existsByPhoneNumber(phone) ||
+     *                 studentRepository.existsByPhoneNumber(phone) || teacherRepository.existsByPhoneNumber(phone) ||
+     *                 viceDeanRepository.existsByPhoneNumber(phone) || guestUserRepository.existsByPhoneNumber(phone)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_PHONE_NUMBER, phone));
+     *         } else if (studentRepository.existsByEmail(email) || teacherRepository.existsByEmail(email)) {
+     *             throw new ConflictException(String.format(Messages.ALREADY_REGISTER_MESSAGE_EMAIL, email));
+     *         }
+     *     }
+     *
+     */
 
     //Not: getAll() *********************************************************************************************************************************
     public Page<Admin> getAllAdmin(Pageable pageable) {
-
         return adminRepository.findAll(pageable);
-
     }
 
     //Not: delete() *********************************************************************************************************************************
@@ -167,10 +130,8 @@ public class AdminService {
 
         if (admin.isPresent()) {
             adminRepository.deleteById(id);
-
             return "Admin is deleted successfully";
         }
-
         return Messages.NOT_FOUND_USER_MESSAGE;
     }
 
