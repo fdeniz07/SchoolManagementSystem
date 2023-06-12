aa1056298da17090c9b9d2e53c47fde79bd34f71
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-12 23:10:45 +0200

    Day 16 - Student Controller-Service-Repository

--- src/main/java/com/schoolmanagement/repository/StudentRepository.java
+++ src/main/java/com/schoolmanagement/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.schoolmanagement.repository;
 
 import com.schoolmanagement.entity.concretes.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
@@ -14,4 +15,23 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     boolean existsByEmail(String email);
 
     Student findByUsernameEquals(String username);
+
+    @Query(value = "SELECT (count(s)>0) FROM Student s") //Student tablosunda bir ögrenci var mi
+    boolean findStudent();
+
+    @Query(value = "SELECT MAX(s.studentNumber) FROM Student s") // Student tablosundaki en yüksek numarali numarayi aliyoruz
+    int getLastStudentNumber();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
