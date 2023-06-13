e7de2dfcca3ae1b761da443de075ed27e8d246dc
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-13 22:54:05 +0200

    Day 17 - Student Controller-Service-Repository

--- src/main/java/com/schoolmanagement/repository/StudentRepository.java
+++ src/main/java/com/schoolmanagement/repository/StudentRepository.java
@@ -1,10 +1,14 @@
 package com.schoolmanagement.repository;
 
 import com.schoolmanagement.entity.concretes.Student;
+import com.schoolmanagement.payload.response.StudentResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
     boolean existsByUsername(String usernname);
 
@@ -16,12 +20,35 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
 
     Student findByUsernameEquals(String username);
 
-    @Query(value = "SELECT (count(s)>0) FROM Student s") //Student tablosunda bir ögrenci var mi
+    @Query(value = "SELECT (count(s)>0) FROM Student s")
+        //Student tablosunda bir ögrenci var mi
     boolean findStudent();
 
-    @Query(value = "SELECT MAX(s.studentNumber) FROM Student s") // Student tablosundaki en yüksek numarali numarayi aliyoruz
+    @Query(value = "SELECT MAX(s.studentNumber) FROM Student s")
+        // Student tablosundaki en yüksek numarali numarayi aliyoruz
     int getLastStudentNumber();
+
+    List<Student> getStudentByNameContaining(String studentName);
+
+    Optional<Student> findByUsername(String username); //OrElseThrow icin null kontrolü yapmak icin Optional ekledik
+
+    @Query(value = "SELECT s FROM Student s WHERE s.advisorTeacher.teacher.username =:username") //ilgili ögrencinin advisorTeacher tablosuna git,
+    //oradaki teacherId ile teacher tablosuna git ve ilgili teacher'in username'ini getir diyerek 3 tablo arasinda join kurmus oluyoruz.--> JPQL ile
+    //@Query(value="SELECT s FROM Student s JOIN s.advisorTeacher at JOIN at.teacher t WHERE t.username=:username") --> SQL dilinde yazimi
+    List<Student> getStudentByAdvisorTeacher_Username(String username);
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
