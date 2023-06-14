866438382036b79da3e2c62dad1660afd8bd70c4
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-14 22:33:52 +0200

    Day 18 - StudentInfo Controller-Service-Repository

---
+++ src/main/java/com/schoolmanagement/controller/StudentInfoController.java
@@ -0,0 +1,59 @@
+package com.schoolmanagement.controller;
+
+import com.schoolmanagement.payload.request.StudentInfoRequestWithoutTeacherId;
+import com.schoolmanagement.payload.response.ResponseMessage;
+import com.schoolmanagement.payload.response.StudentInfoResponse;
+import com.schoolmanagement.service.StudentInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/studentInfo")
+@RequiredArgsConstructor
+public class StudentInfoController {
+
+    private final StudentInfoService studentInfoService;
+
+    //Not: save() *******************************************************************************************************************************
+    @PreAuthorize("hasAnyAuthority('TEACHER')")
+    @PostMapping("/save") //http://localhost:8080/studentInfo/save
+    public ResponseMessage<StudentInfoResponse> save(HttpServletRequest httpServletRequest,
+                                                     @RequestBody @Valid StudentInfoRequestWithoutTeacherId request) {
+
+        String username = (String) httpServletRequest.getAttribute("username");
+        return studentInfoService.save(username, request);
+    }
+
+    // Not: delete()*****************************************************************************************************************************
+    @PreAuthorize("hasAnyAuthority('ADMIN','TEACHER')")
+    @DeleteMapping("/delete/{studentInfoId}")
+    public ResponseMessage<?> delete(@PathVariable Long studentInfoId) {
+        return studentInfoService.deleteStudentInfo(studentInfoId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
